68c9c7801c0f9c9317ea2afaae62a8c8cb42ba2b
Sho Yamada <sho.yamada@omicale.com>
2019-01-16 15:20:49 +0900

    cssnano & autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 gulp.task("sass", function() {
   return gulp
     .src("assets/css/*.scss")
     .pipe(sass())
+    .pipe(postcss([
+        autoprefixer(), cssnano()
+    ]))
     .pipe(gulp.dest("dist/css/"));
 });
