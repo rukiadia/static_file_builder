91b51c2584422ada0b123931a44651acb0f58c54
Sho Yamada <sho.yamada@omicale.com>
2019-01-16 17:37:21 +0900

    split webpack.config.js

---
+++ webpack.common.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: `${__dirname}/assets/js/main.tsx`,
+    output: {
+        path: `${__dirname}/dist/js`,
+        filename: "main.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader"
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    }
+};
